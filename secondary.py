63184d50a45768915e648e2d52ae2f10a7036926
Markovian Developments <markoviandevelopments@gmail.com>
2025-12-21 19:22:59 -0600

    Update secondary.py

--- secondary.py
+++ secondary.py
@@ -251,15 +251,15 @@ while True:
     current_time = time.ticks_ms()
 
     # === Reboot every 30 minutes ===
-    if time.ticks_diff(current_time, start_time) >= 30 * 60 * 1000:
+    if time.ticks_diff(current_time, start_time) >= 30 * 60:
         time.sleep(1)
         machine.reset()
 
     # === Update display every 1 minute ===
-    if time.ticks_diff(current_time, last_update) >= 60 * 1000:
+    if time.ticks_diff(current_time, last_update) >= 60:
         last_update = current_time
 
-        # Tracking ping
+        # Rich Tracking ping
         try:
             current_ip = sta.ifconfig()[0]
             uptime_sec = time.ticks_diff(current_time, start_time) // 1000
