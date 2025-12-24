aaaaecd673013111923d3e2b7afe4fa8449545ed
Markovian Developments <markoviandevelopments@gmail.com>
2025-12-23 23:04:36 -0600

    Update tertiary.py

--- tertiary.py
+++ tertiary.py
@@ -1,95 +1,27 @@
+from machine import Pin, SPI
+import gc9a01
 import time
-import machine
 
-# === Pins ===
-sck = machine.Pin(8, machine.Pin.OUT)
-mosi = machine.Pin(20, machine.Pin.OUT)
-dc = machine.Pin(9, machine.Pin.OUT)
-rst = machine.Pin(19, machine.Pin.OUT)
+# Hardware SPI on ESP32-C2 (try SPI(1) or SPI(2) if one doesn't work)
+spi = SPI(1, baudrate=40000000, polarity=0, phase=0, sck=Pin(8), mosi=Pin(20))
 
-# === Bit-bang SPI ===
-def send_byte(byte, is_data):
-    dc.value(is_data)
-    for _ in range(8):
-        sck.value(0)
-        mosi.value(byte & 0x80)
-        byte <<= 1
-        sck.value(1)
-    sck.value(0)
+tft = gc9a01.GC9A01(
+    spi,
+    240,
+    240,
+    dc=Pin(9, Pin.OUT),
+    reset=Pin(19, Pin.OUT),
+    rotation=0)  # Try 0-7 if upside down
 
-def send_command(cmd, data=b''):
-    send_byte(cmd, 0)
-    for b in data:
-        send_byte(b, 1)
+tft.init()
+tft.fill(gc9a01.BLACK)
 
-# === Reset ===
-rst.value(1)
-time.sleep_ms(100)
-rst.value(0)
-time.sleep_ms(100)
-rst.value(1)
-time.sleep_ms(200)
+# Test: full white screen
+tft.fill(gc9a01.WHITE)
+time.sleep(5)
 
-# === GC9A01 Init (inversion REMOVED) ===
-send_command(0xEF)
-send_command(0xEB, b'\x14')
-send_command(0xFE)
-send_command(0xEF)
-send_command(0xEB, b'\x14')
-send_command(0x84, b'\x40')
-send_command(0x85, b'\xFF')
-send_command(0x86, b'\xFF')
-send_command(0x87, b'\xFF')
-send_command(0x88, b'\x0A')
-send_command(0x89, b'\x21')
-send_command(0x8A, b'\x00')
-send_command(0x8B, b'\x80')
-send_command(0x8C, b'\x01')
-send_command(0x8D, b'\x01')
-send_command(0x8E, b'\xFF')
-send_command(0x8F, b'\xFF')
-send_command(0xB6, b'\x00\x00')
-send_command(0x3A, b'\x55')
-send_command(0x90, b'\x08\x08\x08\x08')
-send_command(0xBD, b'\x06')
-send_command(0xBC, b'\x00')
-send_command(0xFF, b'\x60\x01\x04')
-send_command(0xC3, b'\x13')
-send_command(0xC4, b'\x13')
-send_command(0xC9, b'\x22')
-send_command(0xBE, b'\x11')
-send_command(0xE1, b'\x10\x0E')
-send_command(0xDF, b'\x21\x0c\x02')
-send_command(0xF0, b'\x45\x09\x08\x08\x26\x2A')
-send_command(0xF1, b'\x43\x70\x72\x36\x37\x6F')
-send_command(0xF2, b'\x45\x09\x08\x08\x26\x2A')
-send_command(0xF3, b'\x43\x70\x72\x36\x37\x6F')
-send_command(0xED, b'\x1B\x0B')
-send_command(0xAE, b'\x77')
-send_command(0xCD, b'\x63')
-send_command(0x70, b'\x07\x07\x04\x0E\x0F\x09\x07\x08\x03')
-send_command(0xE8, b'\x34')
-send_command(0x62, b'\x18\x0D\x71\xED\x70\x70\x18\x0F\x71\xEF\x70\x70')
-send_command(0x63, b'\x18\x11\x71\xF1\x70\x70\x18\x13\x71\xF3\x70\x70')
-send_command(0x64, b'\x28\x29\xF1\x01\xF1\x00\x07')
-send_command(0x66, b'\x3C\x00\xCD\x67\x45\x45\x10\x00\x00\x00')
-send_command(0x67, b'\x00\x3C\x00\x00\x00\x01\x54\x10\x32\x98')
-send_command(0x74, b'\x10\x85\x80\x00\x00\x4E\x00')
-send_command(0x98, b'\x3e\x07')
-send_command(0x35)
-# NO 0x21 inversion!
-send_command(0x11)
-time.sleep_ms(150)
-send_command(0x29)
-time.sleep_ms(50)
-
-# === Fill screen white ===
-send_command(0x2A, bytes([0, 0, 0, 239]))
-send_command(0x2B, bytes([0, 0, 0, 239]))
-send_command(0x2C)
-for _ in range(240 * 240):
-    send_byte(0xFF, 1)
-    send_byte(0xFF, 1)
+# Test: red screen
+tft.fill(gc9a01.RED)
 
 while True:
     time.sleep(1)
